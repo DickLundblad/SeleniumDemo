f676d942782fdc3261c58394da66ce0f9bda5abc
Dick <dick.lundblad@gmail.com>
2025-04-14 13:38:47 +0200

    update tst method for UpdateJobListings

--- ResultFileOperationsTests.cs
+++ ResultFileOperationsTests.cs
@@ -16,7 +16,7 @@ namespace SeleniumDemo
         public void CreateJobListings() 
         {
             var randomName = Guid.NewGuid().ToString(); // Generate a random name
-            var fileName = $"SaveJobListingsToFile_{randomName}.tsv";
+            var fileName = $"CreateJobListings_{randomName}.tsv";
             List<JobListing> jobListings = new List<JobListing>();
             var job1 = new JobListing() { JobLink = "https://www.linkedin.com/jobs/collections/it-services-and-it-consulting/?currentJobId=4170820433" };
             jobListings.Add(job1);
@@ -29,7 +29,7 @@ namespace SeleniumDemo
         public void ReadJobListings() 
         {
             var randomName = Guid.NewGuid().ToString(); // Generate a random name
-            var fileName = $"SaveJobListingsToFile_{randomName}.tsv";
+            var fileName = $"ReadJobListings_{randomName}.tsv";
             var jobLink = "https://www.linkedin.com/jobs/collections/it-services-and-it-consulting/?currentJobId=4170820433";
             List<JobListing> jobListings = new List<JobListing>();
             var job1 = new JobListing() { JobLink = jobLink};
@@ -48,22 +48,24 @@ namespace SeleniumDemo
         public void UpdateJobListings()
        { 
             var randomName = Guid.NewGuid().ToString(); // Generate a random name
-            var fileName = $"SaveJobListingsToFile_{randomName}.tsv";
+            var fileName = $"UpdateJobListings_{randomName}.tsv";
             var jobLink = "https://www.linkedin.com/jobs/collections/it-services-and-it-consulting/?currentJobId=4170820433";
-            var updatedJobLink = "https://www.linkedin.com/jobs/collections/it-services-and-it-consulting/?currentJobId=4170820433";
+            var updatedJobLink = "https://www.linkedin.com/jobs/updatedlink";
             List<JobListing> jobListings = new List<JobListing>();
             var job1 = new JobListing() { JobLink = jobLink};
             jobListings.Add(job1);
             SeleniumTestsHelpers.WriteToFile(jobListings, fileName);
+
             List<JobListing> jobListingsFromFile = SeleniumTestsHelpers.LoadJobListingsFromFile(fileName);
             jobListingsFromFile[0].JobLink = updatedJobLink;
-            SeleniumTestsHelpers.WriteToFile(jobListings, fileName);
-            List<JobListing> updateJobListingsFromFile = SeleniumTestsHelpers.LoadJobListingsFromFile(fileName);
+            SeleniumTestsHelpers.WriteToFile(jobListingsFromFile, fileName);
 
-            Assert.That(jobListingsFromFile.Count, Is.EqualTo(jobListings.Count), "The re-loaded JobListing from file does not contain the same number of objects.");
-            Assert.That(jobListingsFromFile[0].JobLink, Is.EqualTo(updatedJobLink), "The re-loaded JobListing from file does not contain the same object.");
-            Assert.That(jobListingsFromFile, Is.EquivalentTo(jobListings).Using(new JobListingComparer()), 
-            "The re-loaded updated JobListings from file do not match. the jobLink is used for comparison ");
+            List<JobListing> updatedJobListingsFromFile = SeleniumTestsHelpers.LoadJobListingsFromFile(fileName);
+
+            Assert.That(updatedJobListingsFromFile.Count, Is.EqualTo(jobListings.Count), "The re-loaded JobListing from file does not contain the same number of objects.");
+            Assert.That(updatedJobListingsFromFile[0].JobLink, Is.EqualTo(updatedJobLink), "The re-loaded JobListing from file does not contain the same object.");
+            Assert.That(updatedJobListingsFromFile, Is.Not.EquivalentTo(jobListings).Using(new JobListingComparer()), 
+            "The re-loaded updated JobListings from file should notmatch. the jobLink is used for comparison ");
         }
 
         // update objects in file  CRUD(Write, Read, update, Write, read) 
