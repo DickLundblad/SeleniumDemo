f8b97c220db442ddebaabc6b41018ec13296f99c
Dick <dick.lundblad@gmail.com>
2025-04-28 14:06:48 +0200

    add installation project and a WPF app

---
+++ JobCrawlerWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,177 @@
+﻿using System.Windows;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.VisualStudio.PlatformUI;
+using OpenQA.Selenium;
+using System.Globalization;
+using WebCrawler;
+using static JobListingsApi;
+using System.IO;
+using System.Data;
+using Microsoft.Win32;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+using System;
+using System.Windows.Controls;
+
+
+namespace JobCrawlerWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class CrawlItem : INotifyPropertyChanged
+        {
+
+            private bool _isSelected;
+            public bool IsSelected     
+            {
+                get => _isSelected;
+                set
+                {
+                    _isSelected = value;
+                    OnPropertyChanged();
+                }
+            }
+            public string Url { get; set; }
+            public string SelectorXPathForJobEntry { get; set; }
+            public string FileName { get; set; }
+            public string AddDomainToJobPaths { get; set; }
+            public int DelayUserInteraction { get; set; }
+            public bool RemoveParamsInJobLinks { get; set; }
+
+
+            public event PropertyChangedEventHandler? PropertyChanged;
+            protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public ObservableCollection<CrawlItem> CsvData { get; } = new ObservableCollection<CrawlItem>();
+        private JobListingsApi _api;
+        private const string FixedCsvPath = @"Resources/JobCrawlSites.csv";
+
+        const char RESULT_FILE_COLUMN_SEPARATOR = ';';
+        
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            //IWebDriver driverToUse = ChromeDebugger.StartChromeInDebugMode();
+            //_api  = new JobListingsApi(driverToUse);
+            //LoadSitesToCrawl();
+            DataContext = this;
+            CsvDataGrid.ItemsSource = CsvData;
+            //StatusLabel.Text = "Ready";
+        }
+
+        private void Button_Crawl_Info_Click(object sender, RoutedEventArgs e)
+        {
+            StatusLabel.Text = "Button clicked at " + DateTime.Now.ToString();
+
+            // Load and display CSV data
+           LoadCsvData(FixedCsvPath, RESULT_FILE_COLUMN_SEPARATOR);
+        }
+        private void Button_Chrome_Debug_Click(object sender, RoutedEventArgs e)
+        {
+            IWebDriver driverToUse = ChromeDebugger.StartChromeInDebugMode();
+            _api  = new JobListingsApi(driverToUse);
+        }
+
+        private void Button_Start_Crawl_Click(object sender, RoutedEventArgs e)
+        {
+            LoadSitesToCrawl();
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            // Prevent the click from bubbling up to the row
+            e.Handled = true;
+    
+            // Get the checkbox that was clicked
+            var checkBox = sender as CheckBox;
+            if (checkBox != null && checkBox.DataContext is CrawlItem item)
+            {
+                // The binding will handle the value change, but we can add additional logic here
+                Console.WriteLine($"Checkbox clicked for {item.FileName}, new value: {item.Url}");
+            }
+        }
+
+        private async void LoadSitesToCrawl()
+        { 
+            List<CrawlItem> selectedItems = CsvData.Where(item => item.IsSelected).ToList();
+            
+            foreach (var data in selectedItems)
+            {
+                var timeout = TimeSpan.FromMinutes(2);
+                try
+                {
+                    StatusLabel.Text = "Crawling " + data.Url;
+                    //await _api.CrawlWithProgressAsync(data.Url, data.SelectorXPathForJobEntry, data.FileName, progress, data.AddDomainToJobPaths, data.DelayUserInteraction, data.RemoveParamsInJobLinks);
+ 
+                    //var progress = new Progress<CrawlProgressReport>(async report =>
+                    //{
+                    //    await MainThread.InvokeOnMainThreadAsync(() =>
+                    //    {
+                    //        ProgressBar.Progress = report.Percentage / 100.0; // MAUI uses 0.0-1.0 range
+                    //        ProgressLabel.Text = report.Message;
+                    //    });
+            
+                    //    Console.WriteLine(report.Message);
+                    //});
+                    var progress = new Progress<CrawlProgressReport>();
+                    _api.CrawlStartPageForJoblinks_ParseJobLinks_WriteToFile(data.Url, data.SelectorXPathForJobEntry, data.FileName, data.AddDomainToJobPaths, data.DelayUserInteraction, data.RemoveParamsInJobLinks);
+                    //await _api.CrawlWithProgressAsync(data.Url, data.SelectorXPathForJobEntry, data.FileName, progress,data.AddDomainToJobPaths, data.DelayUserInteraction, data.RemoveParamsInJobLinks);
+                    StatusLabel.Text = "Crawled " + data.FileName;
+                }
+                catch (Exception ex)
+                {
+                    StatusLabel.Text = "Error: " + ex.Message;
+                }
+            }
+        }
+
+        private void LoadCsvData(string filePath, char separator)
+        {
+            CsvData.Clear();
+            
+            string[] lines = File.ReadAllLines(filePath);
+            if (lines.Length == 0) return;
+            // Skip header row if exists
+            int startLine = lines.Length > 0 && lines[0].StartsWith("Url;") ? 1 : 0;
+
+          for (int i = startLine; i < lines.Length; i++)
+                {
+                string[] values = lines[i].Split(separator);
+                
+                var item = new CrawlItem
+                {
+                    // Assign values to fixed columns
+                    Url = values.Length > 0 ? values[0] : string.Empty,
+                    SelectorXPathForJobEntry = values.Length > 1 ? values[1] : string.Empty,
+                    FileName = values.Length > 2 ? values[2] : string.Empty,
+                    AddDomainToJobPaths = values.Length > 2 ? values[3] : string.Empty,
+                    DelayUserInteraction = values.Length > 1 ? int.Parse(values[4]) : 0,
+                    RemoveParamsInJobLinks = values.Length > 4 ? bool.Parse(values[5]) : true,
+                };
+                
+                CsvData.Add(item);
+            }
+        }
+        public class CrawlProgressReport
+        {
+            public string Message { get; }
+            public int Percentage { get; }
+    
+            public CrawlProgressReport(string message, int percentage)
+            {
+                Message = message;
+                Percentage = percentage;
+            }
+       }
+     }
+}
--- Readme.md
+++ Readme.md
@@ -1,5 +1,6 @@
 install.
 
 Chrome must be installed locally
+The application must be started in Administrator mode
 Change sites and conditions by editing JobCrawlSites.csv
 Create installation package or run the NUnit testcases
